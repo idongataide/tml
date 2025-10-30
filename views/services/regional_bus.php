e5eacd512ec885f5d9498dacf16bd42cefb0560f
Idongesit Ataide <idongataide@gmail.com>
2025-10-30 17:41:36 +0100

    first commit

--- views/services/regional_bus.php
+++ views/services/regional_bus.php
@@ -4,10 +4,10 @@
             <div class="row align-items-center small-screen">
                 <div class="col-lg-12 col-sm-12 position-relative page-title-extra-small">
                     <h1 class="mb-15px text-white opacity-7 fw-300 overflow-hidden">
-                        <span class="d-inline-block" data-anime='{ "translateY": [30, 0], "opacity": [0, 1], "easing": "easeOutCubic", "duration": 500, "staggervalue": 300 }'>TML Metroline Services</span>
+                        <span class="d-inline-block" data-anime='{ "translateY": [30, 0], "opacity": [0, 1], "easing": "easeOutCubic", "duration": 500, "staggervalue": 300 }'>TSL Metroline Services</span>
                     </h1>
                     <h2 class="m-auto pb-5px pt-5px t-light fw-600 ls-minus-1px overflow-hidden">
-                        <span class="d-inline-block" data-anime='{ "translateY": [30, 0], "opacity": [0, 1], "easing": "easeOutCubic", "duration": 500, "staggervalue": 300, "delay": 200 }'>Regional Bus Services</span>
+                        <span class="d-inline-block" data-anime='{ "translateY": [30, 0], "opacity": [0, 1], "easing": "easeOutCubic", "duration": 500, "staggervalue": 300, "delay": 200 }'>Regional Coach Services</span>
                     </h2>
                     <a href="https://ihechukwutransport.com/" target="_blank" class="mt-3 btn btn-large btn-primary btn-switch-text btn-box-shadow fw-400">
                         <span> 
@@ -45,11 +45,13 @@
                         </div>
                     </div>
                     <div class="col-lg-8 order-1 order-lg-2 md-mb-50px" data-anime='{ "el": "childs", "translateY": [50, 0], "opacity": [0,1], "duration": 1200, "delay": 0, "staggervalue": 150, "easing": "easeOutQuad" }'>
-                        <h4 class="text-dark-gray fw-600 ls-minus-1px alt-font mb-20px d-block">Beyond Borders: <br/> Seamless Regional Travel at Your Fingertips</h4>
-                        <p>For journeys that extend beyond city limits, TSL Metroline’s Regional Bus Services offer an exceptional solution. Connecting surrounding areas and regions with precision and reliability, we ensure you have access to efficient transportation no matter your destination.</p>
+                        <h4 class="text-dark-gray fw-600 ls-minus-1px alt-font mb-20px d-block">Beyond Borders: <br/> Seamless Regional Travel Across West Africa</h4>
+                        <p>For journeys that go beyond Nigeria’s borders, TSL Metroline’s Regional Coach Services provide a seamless and dependable travel experience across West Africa. We connect major cities and regions with precision and reliability, ensuring you enjoy smooth, efficient transportation wherever your destination may be.</p>
+                        <p>Our fleet comprises exclusively high-capacity luxury coaches, purposefully designed to deliver exceptional comfort and convenience on long-distance trips. Whether you’re traveling for business, leisure, or personal reasons, we make every journey effortless, with attention to safety, timeliness, and passenger comfort.</p>
                         <img src="<?php echo base_url(); ?>/assets/img/bg/slide2.webp" class="mt-30px mb-30px md-mt-15px border-radius-6px" alt="">
-                        <p>Our regional services feature a fleet of high-capacity and medium-capacity buses, thoughtfully designed to provide unparalleled comfort and convenience for longer journeys. Whether traveling for leisure, business, or personal reasons, we prioritize efficiency and minimal travel times, ensuring your experience is as seamless as it is enjoyable. With TSL Metroline, reaching your destination is effortless and refined.</p>
-                  </div>
+                        <p>To enhance your cross-border experience, we also provide immigration support services, ensuring smooth documentation and clearance at entry and exit points.</p>
+                        <p>With TSL Metroline, traveling beyond borders is not just about reaching your destination, it’s about experiencing the journey in comfort, confidence, and style.</p>
+                    </div>
                 </div>
             </div> 
         </section>

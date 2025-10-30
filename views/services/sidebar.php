d6e576b61a0506f040042c67cd6ffa7fe520734f
Idongesit Ataide <idongataide@gmail.com>
2025-10-30 17:42:06 +0100

    first commit

--- views/services/sidebar.php
+++ views/services/sidebar.php
@@ -2,7 +2,7 @@
     <ul class="p-0 m-0 list-style-02">
         <li class="pb-15px mb-15px border-bottom border-color-extra-medium-gray align-items-center"><a href="<?php echo base_url(); ?>/services/urban_mass">Urban Mass Transit</a><i class="feather icon-feather-arrow-right ms-auto"></i></li>
         <li class="pb-15px mb-15px border-bottom border-color-extra-medium-gray align-items-center text-dark-gray"><a href="<?php echo base_url(); ?>/services/intercity_services" class="text-dark-gray text-dark-gray-hover">Intercity Services</a><i class="feather icon-feather-arrow-right ms-auto"></i></li>
-        <li class="pb-15px mb-15px border-bottom border-color-extra-medium-gray align-items-center"><a href="<?php echo base_url(); ?>/services/regional_bus">Regional Bus Services</a><i class="feather icon-feather-arrow-right ms-auto"></i></li>
+        <li class="pb-15px mb-15px border-bottom border-color-extra-medium-gray align-items-center"><a href="<?php echo base_url(); ?>/services/regional_bus">Regional Coach Services</a><i class="feather icon-feather-arrow-right ms-auto"></i></li>
         <li class="pb-15px align-items-center"><a href="<?php echo base_url(); ?>/services/bus_charter">Bus Charter</a><i class="feather icon-feather-arrow-right ms-auto"></i></li>
     </ul>
 </div>
